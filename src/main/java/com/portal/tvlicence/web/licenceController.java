4f76118b3a3e9d8b8ea4ed5fe48000c8da22b467
Peter Gillespie <50038020@belfastmet.ac.uk>
2024-08-30 15:40:10 +0100

    Changed the paths of references in html files to refer to static Added completed to licenceController Updated the licenceController for completed and payment from Get to Post

--- src/main/java/com/portal/tvlicence/web/licenceController.java
+++ src/main/java/com/portal/tvlicence/web/licenceController.java
@@ -3,6 +3,7 @@ package com.portal.tvlicence.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class licenceController {
@@ -33,12 +34,20 @@ public class licenceController {
 		return "inputdetails";
 	}
 
-	@GetMapping("/payment")
+	@PostMapping("/payment")
 	public String isPayment(Model theModel) {
 
 		theModel.addAttribute("theDate", new java.util.Date());
 
 		return "payment";
+	}
+
+	@PostMapping("/completed")
+	public String isCompleted(Model theModel) {
+
+		theModel.addAttribute("theDate", new java.util.Date());
+
+		return "completed";
 	}
 }
 
