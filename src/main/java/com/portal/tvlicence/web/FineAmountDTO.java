a86e839748f5795d8a12b687e48e9242d250f414
Sergey <skravtsov1@yahoo.com>
2024-09-01 22:27:48 +0100

    Adding code from Mark's project. An intermediate step where I start adding code for an object to pass between pages.

--- src/main/java/com/portal/tvlicence/web/FineAmountDTO.java
+++ src/main/java/com/portal/tvlicence/web/FineAmountDTO.java
@@ -1,30 +1,89 @@
 package com.portal.tvlicence.web;
 
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Pattern;
+
 public class FineAmountDTO {
 
     private String reference;
-    private double amount;
+    private String house;
+    private String street;
+    private String city;
+    private String postcode;
+    private double fine;
+    private String firstName;
+    private String lastName;
+    private String email;
+
 
     // Default constructor is needed for JSON parsing
     public FineAmountDTO() {
     }
 
     // Getter and setter methods
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 
     public String getReference() {
         return reference;
     }
-
     public void setReference(String reference) {
         this.reference = reference;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getAmount() {
+        return fine;
+    }
+    public void setAmount(double amount) {
+        this.fine = amount;
     }
 }
 
--- src/main/java/com/portal/tvlicence/web/licenceController.java
+++ src/main/java/com/portal/tvlicence/web/licenceController.java
@@ -1,8 +1,12 @@
 package com.portal.tvlicence.web;
 
+import com.portal.tvlicence.entity.Licence;
+import com.portal.tvlicence.service.licenceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -10,18 +14,8 @@ public class licenceController {
 
 	// create a mapping for "/hello"
 
-	@GetMapping("/hello")
-	public String sayHello(Model theModel) {
-
-		theModel.addAttribute("theDate", new java.util.Date());
-
-		return "helloworld";
-	}
-
 	@GetMapping("/main")
 	public String isMain(Model theModel) {
-
-//		theModel.addAttribute("theDate", new java.util.Date());
 
 		return "main";
 	}
@@ -48,6 +42,30 @@ public class licenceController {
 //		theModel.addAttribute("theDate", new java.util.Date());
 
 		return "completed";
+	}
+
+
+
+
+
+	@Autowired
+	private licenceService liceService;
+
+	@PostMapping("/amendAmount")
+	public String displayFineForm(@ModelAttribute FineAmountDTO fineAmountDTO, Model model) {
+		Licence theLicence = liceService.findByReference(fineAmountDTO.getReference());
+
+		if (theLicence != null) {
+			FineAmountDTO populatedDTO = new FineAmountDTO();
+			populatedDTO.setReference(theLicence.getReference());
+			populatedDTO.setAmount(theLicence.getFine());
+			model.addAttribute("fineAmountDTO", populatedDTO);
+
+			return "fine-form";
+		} else {
+			// Handle case where fine is not found
+			return "search";
+		}
 	}
 }
 
