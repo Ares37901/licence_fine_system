5e3d5f616d346a7950d55ec605444b3618c91ccc
Sergey <skravtsov1@yahoo.com>
2024-09-02 14:55:30 +0100

    SQL files updated

--- lib/add-fine.sql
+++ lib/add-fine.sql
@@ -1,2 +1,2 @@
 INSERT INTO `licence_fine` 
-VALUES (6,'ABC12D456','69','Hottie Avenue','Gotham','BT693FM','300','Sexy','Chick','hotness@example.com')
+VALUES (6,'ABC12D461','69','Hottie Avenue','Gotham','BT693FM','300','Sexy','Chick','hotness@example.com')
--- lib/licence-directory.sql
+++ lib/licence-directory.sql
@@ -19,7 +19,7 @@ CREATE TABLE `licence_fine` (
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Data for table `licence-fine`
