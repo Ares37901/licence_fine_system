b1f17637d1fc329fa9c3236d0049ed07448afb8d
Sergey <skravtsov1@yahoo.com>
2024-09-03 11:09:46 +0100

    Updated SQL files in lib

--- lib/add-fine.sql
+++ lib/add-fine.sql
@@ -1,2 +1,4 @@
-INSERT INTO `licence_fine` 
-VALUES (6,'ABC12D461','69','Hottie Avenue','Gotham','BT693FM','300','Sexy','Chick','hotness@example.com')
+INSERT INTO `licence_fine` VALUES
+ (2,'ABC12D457','78','Malone Road','Belfast','BT95BW','1000','Marie','Devlin','MarieAnnDevlin24@gmail.com'),
+ (3,'ABC12D458','15','Raloo Avenue','Larne','BT402BY','1000','Bernie','Bryson','BernieB123@bt.com'),
+ (4,'ABC12D459','43','Arlington Road','London','KT611BZ','1000','Fernando','Garcia','FRGarcia84@aol.com ')
